c209273f261c96c40ec1a20928908918895fc038
Christian Niederreiter <cndreiter@gmail.com>
2015-10-06 14:42:35 +0200

    COPYRIGHT message added to all code files

--- src/lib/participants.js
+++ src/lib/participants.js
@@ -1,3 +1,8 @@
+/* Copyright (C) Christian Niederreiter <cndreiter@gmail.com>
+ * Unauthorized copying of this file, via any medium, is strictly prohibited
+ * Proprietary and confidential
+ */
+
 'use strict'
 
 module.exports = 'lib/daytris-participants'
